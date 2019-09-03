f8aedbd937ceb7f1b3eda68741c8ca315cd1f0c5
Jeremy Diaz <j.jeremy.diaz@berkeley.edu>
2019-09-03 00:38:45 -0700

    Added accessibility service overlay

--- pickupautomator/app/src/main/java/io/kotati/pickup_automator/MainActivity.kt
+++ pickupautomator/app/src/main/java/io/kotati/pickup_automator/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.kotati.pickup_automator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Start Service
+        Intent()
+        val serviceIntent = Intent(this,  AutomatorService::class.java)
+        if (this != null) {
+            this.startService(serviceIntent)
+        }
     }
 }
